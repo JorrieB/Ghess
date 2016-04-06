1a229d50724fdd3346ccf80f8b8c922983f14334
Nayeon Kim <nayeonk@mit.edu>
2016-04-06 16:59:19 -0400

    new start view

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,9 @@
 $(function() {
 	var start_view = nunjucks.render('/templates/start_view.html');
     var play_view = nunjucks.render('/templates/play_view.html');
-
     $('body').append($(start_view));
+    $('#player-button').click(function () {
+    	$('body').replaceWith($(play_view));
+    });
+    
 });
