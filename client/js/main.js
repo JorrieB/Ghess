556f176b67595ff8d09fb885f524e12c44e38887
Nayeon Kim <nayeonk@mit.edu>
2016-04-07 18:14:49 -0400

    button styling

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,21 @@
 $(function() {
 	var start_view = nunjucks.render('/templates/start_view.html');
     var play_view = nunjucks.render('/templates/play_view.html');
+	var spectator_view = nunjucks.render('/templates/spectator_view.html');
+
+	// Initialize with start_view
     $('body').append($(start_view));
+
     $('#player-button').click(function () {
     	$('body').replaceWith($(play_view));
     });
+
+    $('#spectator-button').click(function () {
+    	$('body').replaceWith($(spectator_view));
+    });
     
+    $('#forefeit-button').click(function () {
+        console.log('click!');
+    	$('body').replaceWith($(start_view));
+    });
 });
