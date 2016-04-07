4956bb8104ae4da3f0f1abdd5738871ee90a1352
fmmoret <fmmoret2@gmail.com>
2016-04-07 17:45:08 -0400

    should replace existing screen with new screen instead of body

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,21 @@
 $(function() {
+    ///////////////
+    // SOCKET.IO
+    //////////////
+    var socket = io();
+
+
+
+
+    ///////////////
+    // SCREEN FLOW
+    ///////////////
 	var start_view = nunjucks.render('/templates/start_view.html');
     var play_view = nunjucks.render('/templates/play_view.html');
+
     $('body').append($(start_view));
     $('#player-button').click(function () {
-    	$('body').replaceWith($(play_view));
+    	$('.screen').replaceWith($(play_view));
     });
-    
+
 });
