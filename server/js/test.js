dbe0d5f9a2bf6156012d1140442dec023a8fbe60
Jorrie Brettin <jorriebrettin@gmail.com>
2016-04-11 16:46:35 -0400

    filter implemented

--- server/js/test.js
+++ server/js/test.js
@@ -1,12 +1,12 @@
 var Game = require('../Game');
 var Archer = require('./archer');
 
-var archer = new Archer({x:0, y:1}, {x:0, y:1}, "Eric");
-console.log(archer.getAttackableCells());
+// var archer = new Archer({x:0, y:1}, {x:0, y:1}, "Eric");
+// console.log(archer.getAttackableCells());
 
 
-// var game = new Game();
-// game.addPlayer("Jorrie");
-// game.addPlayer("Giulio");
-// game.staticStart();
-// console.log(game.serialize());
+var game = new Game();
+game.addPlayer("Jorrie");
+game.addPlayer("Giulio");
+game.staticStart();
+game.serialize("Giulio");
